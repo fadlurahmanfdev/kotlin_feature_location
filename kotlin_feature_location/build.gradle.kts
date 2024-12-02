c48a0bd58728c2824d59ec2f2fb3611f881aeadd
fadlurahmanfdev <taufikfadlurahmanfajari.dev@gmail.com>
2024-12-02 13:22:59 +0700

    implement ci-cd

--- kotlin_feature_location/build.gradle.kts
+++ kotlin_feature_location/build.gradle.kts
@@ -1,7 +1,11 @@
+import com.vanniktech.maven.publish.SonatypeHost
+
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.jetbrains.kotlin.android)
     id("maven-publish")
+
+    id("com.vanniktech.maven.publish") version "0.29.0"
 }
 
 android {
@@ -59,6 +63,39 @@ publishing {
             afterEvaluate {
                 from(components["release"])
             }
+        }
+    }
+}
+
+mavenPublishing {
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+    signAllPublications()
+
+    coordinates("com.fadlurahmanfdev", "feature_location", "0.0.1")
+
+    pom {
+        name.set("Feature Remote Location")
+        description.set("Library to simplified Location")
+        inceptionYear.set("2024")
+        url.set("https://github.com/fadlurahmanfdev/kotlin_feature_location/")
+        licenses {
+            license {
+                name.set("The Apache License, Version 2.0")
+                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                distribution.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+            }
+        }
+        developers {
+            developer {
+                id.set("fadlurahmanfdev")
+                name.set("Taufik Fadlurahman Fajari")
+                url.set("https://github.com/fadlurahmanfdev/")
+            }
+        }
+        scm {
+            url.set("https://github.com/fadlurahmanfdev/kotlin_feature_location/")
+            connection.set("scm:git:git://github.com/fadlurahmanfdev/kotlin_feature_location.git")
+            developerConnection.set("scm:git:ssh://git@github.com/fadlurahmanfdev/kotlin_feature_location.git")
         }
     }
 }
